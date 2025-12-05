53f140ae077a2ad837f751ccce37c78af22e998a
Maurizio Piroddi <x.hacker.hiphop99@gmail.com>
2025-12-05 01:21:09 +0100

    Inizio esercizio

---
+++ verificaMail/script.js
@@ -0,0 +1,10 @@
+const listaInvitati = [
+    'giampieroburghers@gmail.com',
+    'jhonnyfresco@gmail.com',
+    'marcosquaglio@gmail.com',
+    'sferaebbasta@gmail.com',
+    'queenelizabeth@protonmail.com',
+    'realcivetta@yahoo.com',
+    'cicciopasticcio@gmail.com',
+    'mrbean@gmail.com'
+];
