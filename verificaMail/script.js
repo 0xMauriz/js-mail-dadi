34a7883cf303ad777f112383df14e1dfcbf36f6d
Maurizio Piroddi <x.hacker.hiphop99@gmail.com>
2025-12-05 01:22:16 +0100

    Chiedo all'utente la sua mail

--- verificaMail/script.js
+++ verificaMail/script.js
@@ -8,3 +8,13 @@ const listaInvitati = [
     'cicciopasticcio@gmail.com',
     'mrbean@gmail.com'
 ];
+
+let isPresent;
+
+// DEBUG
+
+console.log(listaInvitati);
+
+// Chiedo all'utente di fornire la sua mail
+
+let personMail = prompt("Inserisci la tua mail per verificare se sei invitato: ");
