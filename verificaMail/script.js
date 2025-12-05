844f1de4111cc33a88e7f239755f7d6118bb0f33
Maurizio Piroddi <x.hacker.hiphop99@gmail.com>
2025-12-05 01:25:51 +0100

    Assegno alla variabile isPresent il valore booleano "true" se la mail inserita viene trovata nell'array, in seguito se il valore di isPresent corrisponde al valore booleano "true" il programma avvisa l'utente che è invitato tramite una frase, altrimenti se la variabile isPresent non ha valore allora viene stampato un messaggio che avvisa la persona che non è invitata.

--- verificaMail/script.js
+++ verificaMail/script.js
@@ -18,3 +18,17 @@ console.log(listaInvitati);
 // Chiedo all'utente di fornire la sua mail
 
 let personMail = prompt("Inserisci la tua mail per verificare se sei invitato: ");
+
+for (let i = 0; i < listaInvitati.length; i++) {
+    if (listaInvitati[i] === personMail) {
+        isPresent = true;
+    }
+}
+
+if (isPresent === true) {
+        console.log("Ci sei broski, entra pure!");
+}
+
+else {
+    console.log("Non puoi entrare broski :(");
+}
